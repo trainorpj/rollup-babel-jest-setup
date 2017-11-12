d644bca5c1ca7c88b8f042fcad6f5ee3290f6d72
PJ Trainor <trainorpj@gmail.com>
2017-11-11 20:59:02 -0500

    rollup works with cjs and es 😂

---
+++ build/index.es.js
@@ -0,0 +1,36 @@
+var sillyAdd = function sillyAdd(x, y) {
+  var sum = x + y;
+  var expression = x + " + " + y + " = " + sum;
+  return expression;
+};
+
+var sillyMult = function sillyMult(_ref) {
+  var _ref$x = _ref.x,
+      x = _ref$x === undefined ? 0 : _ref$x,
+      _ref$y = _ref.y,
+      y = _ref$y === undefined ? 0 : _ref$y;
+  return {
+    left: x,
+    right: y,
+    product: x * y
+  };
+};
+
+var wowMath = function wowMath(x, y) {
+  var sumExpression = sillyAdd(x, y);
+
+  var _sillyMult = sillyMult({ x: x, y: y }),
+      left = _sillyMult.left,
+      right = _sillyMult.right,
+      product = _sillyMult.product;
+
+  var multExpression = left + " * " + right + " = " + product;
+
+  var allTheMath = sumExpression + " ... and " + multExpression;
+
+  console.log(allTheMath);
+};
+
+wowMath(7, 5);
+
+export default wowMath;
--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,16 @@ import babel from "rollup-plugin-babel";
 
 export default {
   input: "src/index.js",
-  output: {
-    file: "build/bundle.js",
-    format: "cjs"
-  },
+  output: [
+    {
+      file: "build/index.cjs.js",
+      format: "cjs"
+    },
+    {
+      file: "build/index.es.js",
+      format: "es"
+    }
+  ],
   plugins: [
     resolve(),
     babel({
